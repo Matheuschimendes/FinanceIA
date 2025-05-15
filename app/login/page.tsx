704ae18fb57ebc24b1cf55f5213bf1e4cb6da6a1
Matheus Chimendes <chimendes2013@outlook.com>
2025-05-15 20:32:57 -0300

    feat: add login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ const LoginPage = () => {
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDO */}
       <div className="flex h-full flex-col justify-center max-w-[550px] p-8 mx-auto">
-        <Image src="/logo.svg" alt="Finance AI" width={173} height={39} className="mb-8" />
+        <Image src="/logo.svg" alt="Finance AI " width={173} height={39} className="mb-8" />
         <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
         <p className="text-muted-foreground mb-8">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.
