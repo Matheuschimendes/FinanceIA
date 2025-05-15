b0b5ff5c315d8099fdc671509b55276e8c443001
Matheus Chimendes <chimendes2013@outlook.com>
2025-05-15 20:30:11 -0300

    Primeiro commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./_components/ui/button";
+
+const Home = () => {
+
+  return (
+    <Button>Botao</Button>
+  )
+}
+export default Home;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,9 @@
+const TransactionsPages = () => {
+  return (
+    <div>
+      <h1>Transactions page</h1>
+    </div>
+  );
+}
+
+export default TransactionsPages;
