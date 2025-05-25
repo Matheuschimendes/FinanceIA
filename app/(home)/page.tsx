3c3f216881d5d93997d052ef1dba82161d328e34
Matheus Chimendes <chimendes2013@outlook.com>
2025-05-25 10:25:18 -0300

    feat: ajuste de data

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -24,7 +24,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
 
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
-    redirect(`?month=${new Date().getMonth() + 1}`);
+    redirect(`?month=0${new Date().getMonth() + 1}`);
   }
 
   const dashboard = await getDashboard(month);
